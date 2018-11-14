f5b587a204f0e2fa0d3be8e5f4c3d0901762f845
EDWARD YANG <eddiecody888@gmail.com>
2018-11-14 15:56:14 +0000

    hellojs exercise with 'sayHello' funciton completed

---
+++ hello.js
@@ -0,0 +1,12 @@
+
+function sayHello() {
+  console.log("Hello, world");
+}
+
+sayHello();
+
+function sayHello(name){
+  console.log("Hello,"+ name);
+};
+sayHello("sakdhfjkafdhsfkj");
+sayHello("ooo");
