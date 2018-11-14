6628e55b3a6718d251e4831a914c77c474ca1a44
EDWARD YANG <eddiecody888@gmail.com>
2018-11-14 17:56:57 +0000

    Adds conditionals exercises

---
+++ go-outside.js
@@ -0,0 +1,31 @@
+var raining = true;
+var cold = false;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!");
+
+var isCitizen = true;
+var age = 99;
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+};
+
+
+
+
+let temperature = 99;
+
+if (temperature < 100 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+};
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+};
